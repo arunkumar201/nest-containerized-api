b29de9cd7439731d57dafada64db3229d6bc5fb6
arunp <arunprajapati9825@gmail.com>
2025-08-27 12:28:03 +0530

    feat: initialize nestjs project with docker support and eslint configuration

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    {},
+  );
+  await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
